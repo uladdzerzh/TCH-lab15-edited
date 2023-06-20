fa50c23ce71742e0f28665599764652235b0df23
buter <vderzhanovich@gmail.com>
2023-06-20 10:21:07 +0200

    main

---
+++ html/index.php
@@ -0,0 +1,12 @@
+ <title>Moja aplikacja PHP</title>
+</head>
+<body>
+    <h1>Witaj w mojej aplikacji PHP!</h1>
+    <p>Strona startowa aplikacji.</p>
+    <?php
+    
+        $name = "Uladzislau Dzerzhanovich";
+        echo "Witaj, $name!";
+    ?>
+</body>
+</html>
